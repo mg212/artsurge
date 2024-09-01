9b709bf8897181ce089f55c69ed24d4a751c229c
“mg212” <mgonchar@live.com>
2024-09-01 00:06:53 -0400

    Initial commit

---
+++ src/main/java/com/example/artsurge/ArtSurgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.artsurge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSurgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSurgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artsurge/ArtSurgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artsurge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSurgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
