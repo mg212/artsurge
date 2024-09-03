1ee207aa706b079ba78e548163ebbecfe61fe4f2
“mg212” <mgonchar@live.com>
2024-09-03 03:06:27 -0400

    Committed 4

--- src/main/java/com/example/artsurge/config/SecurityConfiguration.java
+++ src/main/java/com/example/artsurge/config/SecurityConfiguration.java
@@ -19,35 +19,6 @@ public class SecurityConfiguration  {
     @Autowired
     private UserServiceImpl userService;
 
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers(
-                            "/registration**",
-                            "/js/**",
-                            "/css/**",
-                            "/img/**",
-                            "/webjars/**").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                        .loginPage("/login")
-                            .permitAll()
-                .and()
-                    .logout()
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/login?logout")
-                .permitAll()
-                // Handle logout
-                .and()
-                .logout().invalidateHttpSession(true).clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/logoutSuccess").permitAll();
-    }
- */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("===========>IN filterChain() ");
---
+++ src/main/java/com/example/artsurge/controller/ArtworkController.java
@@ -0,0 +1,4 @@
+package com.example.artsurge.controller;
+
+public class ArtworkController {
+}
