5d60687e9adf27d6b8191fd3bd9eab1191f4c064
“mg212” <mgonchar@live.com>
2024-09-03 01:23:10 -0400

    Updates

---
+++ src/main/java/com/example/artsurge/repo/BuyerRepository.java
@@ -0,0 +1,14 @@
+package com.example.artsurge.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.example.artsurge.model.Buyer;
+
+
+
+
+public interface BuyerRepository extends JpaRepository<Buyer, Long>{
+
+
+}
