41f8588c01b78438609f352def62ee804c371c5d
Max Goncharov <49658798+mg212@users.noreply.github.com>
2024-09-03 01:04:13 -0400

    Updated

--- src/main/java/com/example/artsurge/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.artsurge.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
